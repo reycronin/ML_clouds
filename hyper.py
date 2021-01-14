0feb5736dd4e50f8556624580a4c63b2021a01bf
Rey <36133157+reycronin@users.noreply.github.com>
2021-01-14 10:31:31 -0700

    3 model runs

--- hyper.py
+++ hyper.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from kerastuner.tuners import RandomSearch
 from kerastuner.engine.hyperparameters import HyperParameters
 import utilities as util
+import pickle
 
 # load data
 data_dir = Path('./data/test')
@@ -17,18 +18,13 @@ train_ds, val_ds = util.allocate_data(list_ds, image_count)
 # set up parallel processing and performance
 train_ds, val_ds = util.configuration(train_ds, val_ds)
 
-num_epochs = 1
-N_EPOCH_SEARCH = 1 
-HYPERBAND_MAX_EPOCHS = 40
-MAX_TRIALS = 20
-EXECUTION_PER_TRIAL = 2
-BAYESIAN_NUM_INITIAL_POINTS = 1
+num_epochs = 75
 LOG_DIR = f"{int(time.time())}"
 
 tuner = RandomSearch(
 	util.build_hypermodel,
 	objective='val_accuracy',
-	max_trials=1,  # how many model variations to test?
+	max_trials=25,  # how many model variations to test
 	executions_per_trial=1,  # how many trials per variation? (same model could perform differently)
 	directory=LOG_DIR)
 
@@ -44,5 +40,6 @@ with open(f"tuner_{int(time.time())}.pkl", "wb") as f:
 
 #model.save("cloud_model_" + str(num_epochs) + ".h5")
 #util.plot_results(history, num_epochs)
+
 
 
